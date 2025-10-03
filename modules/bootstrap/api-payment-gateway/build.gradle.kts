8d116cdd6ad7f06f8a9979809a7d5f566fb3d7b7
kkj <48651886+AAgami@users.noreply.github.com>
2025-10-03 23:39:46 +0900

    chore: 라인, 로깅 세팅 변경 및 Swagger UI 제공을 위한 spring-doc 의존성 추가

--- modules/bootstrap/api-payment-gateway/build.gradle.kts
+++ modules/bootstrap/api-payment-gateway/build.gradle.kts
@@ -13,10 +13,13 @@ dependencies {
     implementation(projects.modules.external.pgClient)
     implementation(libs.spring.boot.starter.jpa)
     implementation(libs.bundles.bootstrap)
+    // API 문서 & 테스트용 Swagger UI 사용을 위한 추가 (springdoc)
+    implementation(libs.springdoc.openapi.starter.webmvc.ui)
+    // 개발 단계에서 이용하기 위해 testImplementation -> implementation로 변경
+    implementation(libs.database.h2)
     testImplementation(libs.bundles.test)
     testImplementation(libs.spring.boot.starter.test) {
         exclude(module = "mockito-core")
     }
     testImplementation(libs.spring.mockk)
-    testImplementation(libs.database.h2)
 }
