02e229099f5b6d8c047dea917725687bd546e62e
kkj <48651886+AAgami@users.noreply.github.com>
2025-10-05 01:26:51 +0900

    chore: testPg 의존성 추가 및 application pg-test 수정

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -11,4 +11,9 @@ dependencies {
     implementation(projects.modules.domain)
     implementation(libs.spring.boot.starter.web)
     implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.springframework:spring-test")
 }
